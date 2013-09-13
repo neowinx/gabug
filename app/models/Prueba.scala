ee66995420d6442d9d083d16e65b5d79f982d5ec
Pedro Flores <pflores@codelab.com.py>
2013-09-13 11:50:11 -0400

    Added personalized logger. Database mapping corrected.

--- app/models/Prueba.scala
+++ app/models/Prueba.scala
@@ -8,19 +8,19 @@ import anorm.SqlParser._
 
 import java.util.Date
 
-case class Prueba(id: Int, descripcion: String, fechaInicio: Date)
+case class Prueba(id: Int, descripcion: String, fechaInicio: Option[Date])
 
 object Prueba {
 
   val simple = {
     get[Int]("prueba.id") ~
       get[String]("prueba.descripcion") ~
-      get[Date]("prueba.fechaInicio") map {
+      get[Option[Date]]("prueba.fechaInicio") map {
       case id~descripcion~fechaInicio=> Prueba(id, descripcion, fechaInicio)
     }
   }
 
-  def findAll(limit: Int = 10, offset: Int = 10): Seq[Prueba] = {
+  def findAll(limit: Int = 10, offset: Int = 0): Seq[Prueba] = {
     DB.withConnection { implicit connection =>
       SQL("select * from prueba limit {limit} offset {offset}")
         .on("limit" -> limit, "offset" -> offset).as(Prueba.simple *)
